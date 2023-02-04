3f26ab3660687a17a4c4f4d04e1472f91eaadd53
YevhenVovk <vovk.valki@ukr.net>
2023-02-04 13:46:24 +0200

    Create models EventType, Event, UUIDModel
    add UUIDModel model which is inherited in Event model;
    add EventType model;
    add Event model with the integer pk column and the uuid column which is had the title 'id';
    make migrations;

--- event_manager/models.py
+++ event_manager/models.py
@@ -1,3 +1,35 @@
+import datetime
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class UUIDModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class EventType(models.Model):
+    name = models.CharField(max_length=254, null=False)
+
+    class Meta:
+        verbose_name = 'Type of the event'
+
+
+class Event(UUIDModel):
+    user = models.ForeignKey(User, null=False, related_name='user', on_delete=models.SET('User was delete'))
+    event_type = models.ForeignKey(EventType, null=False, related_name='event_type', on_delete=models.CASCADE)
+    info = models.JSONField(null=True)
+    timestamp = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(null=False, default=datetime.datetime.now())
+
+    class Meta:
+        ordering = ['event_type', 'timestamp']
+        verbose_name = 'Event'
+
+
--- poly_tech_test/urls.py
+++ poly_tech_test/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from event_manager import models
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
