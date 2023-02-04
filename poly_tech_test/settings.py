bee23dd34452958b1542e910315f70c7dcf1a2d0
YevhenVovk <vovk.valki@ukr.net>
2023-02-04 12:27:24 +0200

    Add the application 'event_manager'
    register application in the settings.py;

--- poly_tech_test/settings.py
+++ poly_tech_test/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'event_manager.apps.EventManagerConfig'
 ]
 
 MIDDLEWARE = [
